fb41e51707b8e4f17cc85036b9212365fa75a7ab
farhan <farhankhanmmb2004@gmail.com>
2024-07-13 19:46:06 +0530

    day 1

---
+++ DAY1/app.js
@@ -0,0 +1,29 @@
+//Activity 1
+    //--task1--
+var a=20;
+console.log(a);
+  //--task2--
+let b=20;
+console.log(b);
+//activity 2
+const flag=true;
+console.log(flag);
+//activity 3//
+let num=3;
+let str="farhan"
+let bool=false;
+let arr=[1,,4]
+let obj={
+    name:'farhan'
+}
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(arr));
+console.log(typeof(obj));
+//activity 5;
+let first=5;
+console.log(first);
+first=10;
+console.log(first);
+flag=false;
