704a6e0cd5b31e03ccb7e08aba205da88601d430
farhan <farhankhanmmb2004@gmail.com>
2024-07-14 07:55:03 +0530

    day2

---
+++ DAY2/app.js
@@ -0,0 +1,32 @@
+//---Activity 1---
+//task1
+let a=20;
+let b=30;
+console.log(a+b);
+//task 2
+console.log(a-b);
+//task3
+console.log(a*b);
+//task4
+console.log(a/b);
+//task5
+console.log(b%a);
+
+//---Activity 2---
+let c=20;
+c+=a;
+console.log(c);
+c-=10;
+console.log(c);
+//---Activity 3--
+console.log(a>b);
+console.log(a>=20);
+console.log(9==9);
+//---activity 4---
+console.log(a>10&&a<25);
+console.log(a>10||a<5);
+console.log(!a<25);
+//---activity 5---
+let num=-1;
+num>0?console.log(true):console.log(false);
+
