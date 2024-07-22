7e9b0a4283a86d9de32441c72a0adcfa921b9c72
farhan <farhankhanmmb2004@gmail.com>
2024-07-22 17:50:19 +0530

    Day9part2

--- DAY9/app.js
+++ DAY9/app.js
@@ -4,14 +4,35 @@ name.style.backgroundColor="yellow"
 console.log(name.innerHTML);
 let group=document.getElementsByClassName("group");
 group[0].style.backgroundColor="pink";
-console.dir(group);
 document.querySelector("body").removeChild(name);
-document.querySelector("body").lastChild.removeChild(name);
+group[0].classList.add("clas");
+console.dir(group[0].classList);
+group[0].classList.remove("group");
+console.dir(document.getElementsByClassName("clas")[0].classList);
+
 }
 let div=document.createElement('div');
 div.textContent="this div is created by JS";
 document.querySelector("body").appendChild(div);
 let li=document.createElement("li");
 li.textContent="list item";
-document.querySelector("ul").append(li);
-//activity 3,4 due
+document.querySelector("ul").prepend(li);
+let list=document.querySelector("ul").children;
+document.querySelector("ul").removeChild(list[list.length-1])
+document.querySelector("img").src="https://images.pexels.com/photos/18749534/pexels-photo-18749534/free-photo-of-man-in-tank-top-throwing-basketball-ball.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load";
+console.dir();
+{
+    let btn=document.querySelector("button");
+    let p=document.querySelector("#discription");
+    let change=()=>{
+        p.textContent="the dicription is changed";
+    }
+    btn.addEventListener("click",change)
+    p.style.display="flex";
+    p.addEventListener("mouseenter",()=>{
+        p.style.border="2px solid black";
+    })
+    p.addEventListener("mouseleave",()=>{
+        p.style.border="";
+    })
+}
